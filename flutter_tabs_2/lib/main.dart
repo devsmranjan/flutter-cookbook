0f7d9616f2a9fa6611cc567ee1842ab28a58ee62
smruti-ranjan-rana <ranaonlive@gmail.com>
2018-03-28 19:00:42 +0530

    Changing Widgets by changing the tabs.

--- flutter_tabs_2/lib/main.dart
+++ flutter_tabs_2/lib/main.dart
@@ -1,38 +1,90 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() {
+  runApp(new MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
-        title: 'Tab Demo',
-        theme: new ThemeData(
-          primarySwatch: Colors.yellow,
+        title: "Tabs Demo",
+        theme: new ThemeData(primarySwatch: Colors.yellow),
+        home: new MyHome());
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  _MyHomeState createState() => new _MyHomeState();
+}
+
+class _MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  final List<Tab> myTabs = <Tab>[
+    new Tab(text: 'LEFT'),
+    new Tab(text: 'CENTER'),
+    new Tab(text: 'RIGHT'),
+  ];
+
+  final List<Icon> _allAppBarIcons = <Icon>[
+    new Icon(Icons.list),
+    new Icon(Icons.search),
+    new Icon(Icons.refresh),
+  ];
+
+  TabController _tabController;
+  Icon _appBarIcons;
+
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, length: myTabs.length);
+    _tabController.addListener(_handleAppBarIcons);
+    _appBarIcons = _allAppBarIcons[0];
+  }
+
+  void _handleAppBarIcons() {
+    setState(() {
+      _appBarIcons = _allAppBarIcons[_tabController.index];
+    });
+  }
+
+  @override
+  void dispose() {
+    _tabController.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        actions: <Widget>[
+          new IconButton(
+              icon: _appBarIcons,
+              onPressed: () {
+                switch(_tabController.index) {
+                  case 0: /*Action for Tab 1*/;
+                  break;
+                  case 1: /*Action for Tab 1*/;
+                  break;
+                  case 2: /*Action for Tab 1*/;
+                  break;
+
+                }
+              }),
+        ],
+        title: new Text("Tabs Demo"),
+        bottom: new TabBar(
+          controller: _tabController,
+          tabs: myTabs,
         ),
-        home: new DefaultTabController(
-            length: 3,
-            child: new Scaffold(
-              appBar: new AppBar(
-                leading: new IconButton(
-                    icon: new Icon(Icons.menu), onPressed: () {}),
-                title: new Text("Tab Demo"),
-                bottom: new TabBar(tabs: [
-                  new Tab(
-                    icon: new Icon(Icons.directions_car),
-                  ),
-                  new Tab(
-                    icon: new Icon(Icons.directions_transit),
-                  ),
-                  new Tab(icon: new Icon(Icons.directions_bike)),
-                ]),
-              ),
-              body: new TabBarView(children: [
-                new Icon(Icons.directions_car),
-                new Icon(Icons.directions_transit),
-                new Icon(Icons.directions_bike),
-              ]),
-            )));
+      ),
+      body: new TabBarView(
+        controller: _tabController,
+        children: myTabs.map((Tab tab) {
+          return new Center(child: new Text(tab.text));
+        }).toList(),
+      ),
+    );
   }
 }
