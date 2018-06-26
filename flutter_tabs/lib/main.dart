904b0c2b05f3c3ac86ae27646de31342ace6bb15
Smruti Ranjan Rana <smrutiranjan.offc@gmail.com>
2018-06-26 19:53:29 +0530

    Flutter Widget

--- flutter_tabs/lib/main.dart
+++ flutter_tabs/lib/main.dart
@@ -33,6 +33,7 @@ class MyApp extends StatelessWidget {
                 new Icon(Icons.directions_transit),
                 new Icon(Icons.directions_bike),
               ]),
-            )));
+            ))
+    );
   }
 }
