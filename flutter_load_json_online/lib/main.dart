bd0143a9f871911810eebadc767750075f897ccb
Smruti Ranjan Rana <smrutiranjan.offc@gmail.com>
2018-09-22 18:56:56 +0530

    no message

--- flutter_load_json_online/lib/main.dart
+++ flutter_load_json_online/lib/main.dart
@@ -1,81 +1,102 @@
 import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:flutter/material.dart';
 
-void main() => runApp(new MyApp());
+void main() => runApp(MyApp());
+
+const _appTitle = 'Computer Science : First Sem';
+
+Future<List<Subjects>> fetchSubjectsData(http.Client client) async {
+  final bputDataUrl = await client.get(
+      'https://raw.githubusercontent.com/smrutiranjanrana/BPUT/master/bput-btech.json');
+
+  // Use the compute function to run parseSubjects in a separate isolate
+  return compute(parseSubjects, bputDataUrl.body);
+}
+
+// A function that will convert a response body into a List<Subjects>
+List<Subjects> parseSubjects(String resposeBody) {
+  final subjectData = json
+      .decode(resposeBody)[1]['semesters'][0]['subjects']
+      .cast<Map<String, dynamic>>();
+
+  return subjectData.map<Subjects>((json) => Subjects.fromJson(json)).toList();
+}
+
+//For Each subjects
+class Subjects {
+  final String subjectName;
+  final int credits;
+
+  Subjects({this.subjectName, this.credits});
+
+  factory Subjects.fromJson(Map<String, dynamic> json) {
+    return Subjects(
+        subjectName: json['subjectName'] as String,
+        credits: json['credits'] as int);
+  }
+}
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'JSON from Network',
-      theme: new ThemeData(
-        primarySwatch: Colors.red,
+    return MaterialApp(
+      title: _appTitle,
+      theme: ThemeData(
+        primarySwatch: Colors.yellow,
       ),
-      home: new MyHomePage(),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   @override
-  _MyHomePageState createState() => new _MyHomePageState();
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_appTitle),
+      ),
+      body: FutureBuilder<List<Subjects>>(
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            print(snapshot.error);
+          }
+
+          return snapshot.hasData
+              ? SubjectsList(subjects: snapshot.data)
+              : Center(
+                  child: CircularProgressIndicator(),
+                );
+        },
+        future: fetchSubjectsData(http.Client()),
+      ),
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  final dataUrl = "https://raw.githubusercontent.com/smruti-ranjan-rana/BPUT/master/bput-btech.json";
-//  final dataUrl = "https://swapi.co/api/people";
-  List data;
+class SubjectsList extends StatelessWidget {
+  final List<Subjects> subjects;
 
-  @override
-  void initState() {
-    super.initState();
-    this.getJSONData();
-  }
-
-  Future getJSONData() async {
-    var response = await http.get(
-//      Encode the Url
-      Uri.encodeFull(dataUrl),
-      headers: {"Accept" : "application/json"}
-        );
-    print(response.body);
-
-    setState(() {
-      var convertDataToJSON = JSON.decode(response.body);
-      data = convertDataToJSON['CE']['semesters'][0]["subjects"];
-      print("Data : $data");
-    });
-  }
+  const SubjectsList({Key key, this.subjects}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(
-      appBar: new AppBar(
-        title: new Text("JSON from Network"),
-      ),
-      body: new ListView.builder(
-        itemBuilder: (context, index) {
-          return new Container(
-            child: new Center(
-              child: new Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: <Widget>[
-                  new Card(
-                    child: new Container(
-                      child: new Text("${data[index]['subjectName']}"),
-                      padding: const EdgeInsets.all(18.0),
-                    ),
-                  )
-                ],
-              ),
-            ),
-          );
-        },
-        itemCount: data == null ? 0 : data.length,
-      ),
+    return ListView.builder(
+      itemBuilder: (context, index) {
+        return ListTile(
+          title: Text(subjects[index].subjectName),
+          trailing: Text(
+            subjects[index].credits.toString(),
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+        );
+      },
+      itemCount: subjects.length,
     );
   }
+
 }
